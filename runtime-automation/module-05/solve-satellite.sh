329b6840aa8802423cdcc9522ada82573d181eb2
Matthew Yee <myee@redhat.com>
2026-08-13 08:49:55 -0700

    Add OpenSCAP client role and STIG sshd tailoring file import to module-05 script

--- runtime-automation/module-05/solve-satellite.sh
+++ runtime-automation/module-05/solve-satellite.sh
@@ -4,4 +4,15 @@ echo "Solving module-05" >> /tmp/progress.log
 # Upload default SCAP contents to Satellite
 hammer scap-content bulk-upload --type default
 
+# Import the OpenSCAP client Ansible role and variables
+hammer ansible roles import --proxy-id 1 --role-names theforeman.foreman_scap_client
+hammer ansible variables import --proxy-id 1
+
+# Import the STIG sshd tailoring file
+hammer tailoring-file create \
+    --name "STIG SSHD Tailoring file" \
+    --scap-file /tmp/rhel10-stig-sshd-tailoring.xml \
+    --organizations "Acme Org" \
+    --locations "Vancouver"
+
 echo "Solved module-05" >> /tmp/progress.log
