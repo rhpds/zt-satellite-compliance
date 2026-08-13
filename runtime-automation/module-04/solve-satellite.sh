d982aabcb4f1497c752ad804a10f5d5437a3151c
Matthew Yee <myee@redhat.com>
2026-08-13 10:43:32 -0700

    Update module documentation and scripts for OpenSCAP compliance configuration

--- runtime-automation/module-04/solve-satellite.sh
+++ runtime-automation/module-04/solve-satellite.sh
@@ -1,27 +1,18 @@
 #!/bin/sh
 echo "Solving module-04" >> /tmp/progress.log
 
-ORG="Acme Org"
+# Upload default SCAP contents to Satellite
+hammer scap-content bulk-upload --type default
 
-# Create host group with content source, lifecycle environment, content view, and OpenSCAP capsule
-hammer hostgroup create \
-  --name "RHEL10" \
-  --organization "$ORG" \
-  --content-source "satellite.lab" \
-  --lifecycle-environment "Library" \
-  --content-view "RHEL10" \
-  --openscap-proxy "satellite.lab"
+# Import the OpenSCAP client Ansible role and variables
+hammer ansible roles import --proxy-id 1 --role-names theforeman.foreman_scap_client
+hammer ansible variables import --proxy-id 1
 
-# Assign the foreman_scap_client Ansible role
-hammer hostgroup ansible-roles assign \
-  --hostgroup "RHEL10" \
-  --ansible-roles "theforeman.foreman_scap_client"
-
-# Set the activation key
-hammer hostgroup set-parameter \
-  --hostgroup "RHEL10" \
-  --name "kt_activation_keys" \
-  --parameter-type "string" \
-  --value "RHEL10"
+# Import the STIG sshd tailoring file
+hammer tailoring-file create \
+    --name "STIG SSHD Tailoring file" \
+    --scap-file /tmp/rhel10-stig-sshd-tailoring.xml \
+    --organizations "Acme Org" \
+    --locations "Vancouver"
 
 echo "Solved module-04" >> /tmp/progress.log
--- runtime-automation/module-05/solve-satellite.sh
+++ runtime-automation/module-05/solve-satellite.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 echo "Solving module-05" >> /tmp/progress.log
 
-# Upload default SCAP contents to Satellite
-hammer scap-content bulk-upload --type default
+ORG="Acme Org"
 
-# Import the OpenSCAP client Ansible role and variables
-hammer ansible roles import --proxy-id 1 --role-names theforeman.foreman_scap_client
-hammer ansible variables import --proxy-id 1
+# Get the Satellite capsule ID for OpenSCAP proxy
+CAPSULE_ID=$(hammer --no-headers capsule list --search "name = satellite.lab" --fields id | tr -d ' ')
 
-# Import the STIG sshd tailoring file
-hammer tailoring-file create \
-    --name "STIG SSHD Tailoring file" \
-    --scap-file /tmp/rhel10-stig-sshd-tailoring.xml \
-    --organizations "Acme Org" \
-    --locations "Vancouver"
+# Create host group with content source, lifecycle environment, content view, and OpenSCAP capsule
+hammer hostgroup create \
+  --name "RHEL10" \
+  --organization "$ORG" \
+  --content-source "satellite.lab" \
+  --lifecycle-environment "Library" \
+  --content-view "RHEL10" \
+  --openscap-proxy-id "$CAPSULE_ID"
+
+# Assign the foreman_scap_client Ansible role
+hammer hostgroup ansible-roles assign \
+  --name "RHEL10" \
+  --ansible-roles "theforeman.foreman_scap_client"
+
+# Set the activation key
+hammer hostgroup set-parameter \
+  --hostgroup "RHEL10" \
+  --name "kt_activation_keys" \
+  --parameter-type "string" \
+  --value "RHEL10"
 
 echo "Solved module-05" >> /tmp/progress.log
