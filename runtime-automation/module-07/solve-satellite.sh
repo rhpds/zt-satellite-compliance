1151c047aa1e412f3430bb78258c136746abcee3
Matthew Yee <myee@redhat.com>
2026-08-14 13:13:18 -0700

    Add second RHEL host (rhel2.lab) and update docs, automation, and UI config

--- runtime-automation/module-07/solve-satellite.sh
+++ runtime-automation/module-07/solve-satellite.sh
@@ -9,7 +9,8 @@ SAT_HOST_REGISTRATION_SCRIPT=$(hammer host-registration generate-command \
   --setup-insights true \
   --setup-remote-execution true)
 
-# Register rhel1 to Satellite
+# Register rhel1 and rhel2 to Satellite
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@rhel1.lab bash -c "$SAT_HOST_REGISTRATION_SCRIPT"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@rhel2.lab bash -c "$SAT_HOST_REGISTRATION_SCRIPT"
 
 echo "Solved module-07" >> /tmp/progress.log
